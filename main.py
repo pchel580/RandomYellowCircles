1a625a526a7c677637ff887488e08b67a0e7887e
pchel580 <neperapk@gmail.com>
2024-12-01 11:15:32 +0800

    first step

---
+++ main.py
@@ -0,0 +1,27 @@
+import sys
+import random
+from PyQt5 import QtWidgets, uic, QtGui
+
+
+class CircleApp(QtWidgets.QWidget):
+    def __init__(self):
+        super(CircleApp, self).__init__()
+        uic.loadUi('UI.ui', self)
+        self.pushButton.clicked.connect(self.draw_circle)
+        self.scene = QtWidgets.QGraphicsScene(self)
+        self.graphicsView.setScene(self.scene)
+
+    def draw_circle(self):
+        diameter = random.randint(20, 100)
+        x = random.randint(0, 400 - diameter)
+        y = random.randint(0, 400 - diameter)
+        circle = QtWidgets.QGraphicsEllipseItem(x, y, diameter, diameter)
+        circle.setBrush(QtGui.QBrush(QtGui.QColor("yellow")))
+        self.scene.addItem(circle)
+
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    window = CircleApp()
+    window.show()
+    sys.exit(app.exec_())
