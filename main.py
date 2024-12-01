757850074063f8b2432b07a6cafd254948c858d7
pchel580 <neperapk@gmail.com>
2024-12-01 11:28:18 +0800

    Git и случайные окружности

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ import random
 from PyQt6 import QtWidgets, uic, QtGui
 
 
-# nothing
-
 class CircleApp(QtWidgets.QWidget):
     def __init__(self):
         super(CircleApp, self).__init__()
@@ -17,8 +15,9 @@ class CircleApp(QtWidgets.QWidget):
         diameter = random.randint(20, 100)
         x = random.randint(0, 400 - diameter)
         y = random.randint(0, 400 - diameter)
+        color = QtGui.QColor(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
         circle = QtWidgets.QGraphicsEllipseItem(x, y, diameter, diameter)
-        circle.setBrush(QtGui.QBrush(QtGui.QColor("yellow")))
+        circle.setBrush(QtGui.QBrush(color))
         self.scene.addItem(circle)
 
 
